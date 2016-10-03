5379b9a893fc725657ceb9238cbeff1944dee20d
mparab01 <infoeastindia1@gmail.com>
2016-10-03 00:48:22 -0500

    Python_Lambda
    https://docs.python.org/3/tutorial/controlflow.html

---
+++ Lambda.py
@@ -0,0 +1,67 @@
+
+# coding: utf-8
+
+# Lambda Expression
+
+# Q. Check is a number is an even number
+
+# In[1]:
+
+even = lambda x : x % 2 ==0
+
+
+# In[2]:
+
+even(5)
+
+
+# In[3]:
+
+even(6)
+
+
+# Q. Find a square of a given number
+
+# In[4]:
+
+square = lambda x : x **2
+
+
+# In[5]:
+
+square(5)
+
+
+# Q. Create an adder function to add two numbers
+
+# In[6]:
+
+adder = lambda x,y:x+y
+
+
+# In[7]:
+
+adder(15,5)
+
+
+# Q. Check length of a string
+
+# In[8]:
+
+st = "Check the length of the string using lambda function"
+
+
+# In[9]:
+
+length = lambda x : len(x)
+
+
+# In[10]:
+
+length(st)
+
+
+# In[ ]:
+
+
+
